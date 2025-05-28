21bd45106393d5b5d6bd697d2232365410ea4f23
kitamoo <ayaka20111005@yahoo.co.jp>
2025-05-28 23:56:18 +0900

    first commit

---
+++ Data/AppicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MinTwitterApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Like> Likes { get; set; }
+}

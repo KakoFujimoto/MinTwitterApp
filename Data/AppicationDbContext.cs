aa6cc19d1e3ab63cd75b935f8fdea199cbf91fea
kitamoo <ayaka20111005@yahoo.co.jp>
2025-05-29 09:47:19 +0900

    LikesテーブルのUser側のカスケード削除を無効に/マイグレーションファイルの作成

--- Data/AppicationDbContext.cs
+++ Data/AppicationDbContext.cs
@@ -11,4 +11,16 @@ public class ApplicationDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Post> Posts { get; set; }
     public DbSet<Like> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Likes の User との関係の削除動作を Restrict に設定
+        modelBuilder.Entity<Like>()
+            .HasOne(l => l.User)
+            .WithMany(u => u.Likes)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }

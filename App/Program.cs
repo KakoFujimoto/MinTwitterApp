5f2d80a85e0fdc2947ee8c27c664b89514fc7b93
kitamoo <ayaka20111005@yahoo.co.jp>
2025-06-02 10:43:33 +0900

    UserServiceをDIコンテナに登録

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinTwitterApp.Data;
+using MinTwitterApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<PasswordService>();
 
 var app = builder.Build();
 

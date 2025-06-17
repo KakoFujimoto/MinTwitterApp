e8777bc7fc5d21a5eae95dfef679d7fe3f4a6c77
kitamoo <ayaka20111005@yahoo.co.jp>
2025-06-17 12:43:46 +0900

    Razorビューの複数の投稿にマウントするように

--- min-twitter-frontend/src/index.js
+++ min-twitter-frontend/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import LikeButton from './components/LikeButton';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.querySelectorAll('[id^="like-button-root-"]').forEach(container => {
+  const userId = parseInt(container.dataset.userId);
+  const postId = parseInt(container.dataset.postId);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const root = createRoot(container);
+  root.render(<LikeButton userId={userId} postId={postId} />);
+});

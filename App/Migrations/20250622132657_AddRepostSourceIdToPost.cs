b20e5a8a3c7cdf32748d1097bd1e942a70debc50
kitamoo <ayaka20111005@yahoo.co.jp>
2025-06-22 22:28:47 +0900

    PostエンティティにRepostSourceIdを追加

---
+++ App/Migrations/20250622132657_AddRepostSourceIdToPost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinTwitterApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRepostSourceIdToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RepostSourceId",
+                table: "Posts",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RepostSourceId",
+                table: "Posts");
+        }
+    }
+}
